9c7d77f03b30c982724c3f86ad53bc632a93a565
Furkan Boncuk <furkanboncuk2000@gmail.com>
2023-06-25 10:23:50 +0300

    Create app.js

---
+++ Temeller/02-Console/app.js
@@ -0,0 +1,13 @@
+console.log("Merhaba Javascript");
+console.log("Merhaba Javascript");
+console.log("Merhaba Javascript");
+
+var a = 20;
+console.log(a);
+
+console.log(["Ahmet","Mehmet"]);
+
+document.getElementById("h3").style.color = "red"
+
+console.warn("Bu bir uyarıdır");
+console.error("Bu bir hatadır");
